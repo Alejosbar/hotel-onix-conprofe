5035c7c0be1918955cc36b48b396ac222b333d71
alejosbar <yazocult@hotmail.com>
2021-04-27 18:59:37 -0500

    Iniciando proyecto para compartir con el profe

---
+++ js/formulariotaller.js
@@ -0,0 +1,9 @@
+function alerta() {
+    var valor_nombre = document.getElementById("nombre").value
+    var valor_apellido = document.getElementById("apellido").value
+    alert("Bienvenid@ " + valor_nombre +" "+ valor_apellido)
+}
+
+function documentos(documentos) {
+    alert(documentos)
+}
